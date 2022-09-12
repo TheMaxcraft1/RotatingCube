4b3d8469e1432821deebda00c0cf1084dd216df2
Máximo Fain <98620343+TheMaxcraft1@users.noreply.github.com>
2022-09-12 19:57:11 -0300

    Program upload

---
+++ RotatingCube.py
@@ -0,0 +1,127 @@
+import pygame
+from pygame.locals import *
+import math
+
+WIDTH = 1280
+HEIGHT = 720
+RESOLUTION = (1280,720)
+angle = 0
+window = pygame.display.set_mode(RESOLUTION)
+clock = pygame.time.Clock()
+
+cube_points = [
+                [[-1],[-1],[-1]], #P0 Every "inside" list is a column
+                [[-1],[1],[-1]],  #P1
+                [[1],[-1],[-1]],  #P2
+                [[1],[1],[-1]],   #P3
+                [[-1],[-1],[1]],  #P4   # Geogebra link https://www.geogebra.org/3d/kmyj3azc 
+                [[-1],[1],[1]],   #P5
+                [[1],[-1],[1]],   #P6
+                [[1],[1],[1]]     #P7
+]
+
+
+
+projection_matrix = [
+                        [1,0,0],
+                        [0,1,0],
+                        [0,0,0]
+                    ]
+
+def connectPoints(i, j, points):
+    pygame.draw.line(window, (255,255,255), (points[i][0], points[i][1]), (points[j][0], points[j][1]))
+
+
+def matrix_multiplication(a,b):
+    
+    a_columns = len(a[0])
+    a_rows = len(a)
+    
+    b_columns = len(b[0])
+    b_rows = len(b)
+
+    if a_columns == b_rows:
+        result_matrix = []
+        for i in range(a_rows):
+            result_matrix.append([])  
+            for k in range(b_columns):
+                result = 0    
+                for j in range(a_columns):
+                    result+= a[i][j] * b[j][k]
+                result_matrix[i].append(result)
+        return result_matrix
+    else:
+        print("No se pueden multiplicar :(")
+
+
+scale = 100
+#Main Loop
+while True:
+    clock.tick(60)
+    window.fill((0,0,0))
+    angle += 0.01
+    
+    rotationX_matrix =  [
+                        [1,0,0],
+                        [0,math.cos(angle), -math.sin(angle)],
+                        [0,math.sin(angle), math.cos(angle)]
+                        ]
+
+    rotationY_matrix =  [
+                        [math.cos(angle),0,math.sin(angle)],
+                        [0,1,0],
+                        [-math.sin(angle),0,math.cos(angle)]    
+                        ]
+
+    rotationZ_matrix =   [
+                        [math.cos(angle),-math.sin(angle),0],
+                        [math.sin(angle), math.cos(angle),0],
+                        [0,0,1]
+                        ]
+    
+    points = [0 for _ in range(len(cube_points))]
+    i = 0
+
+    for point in cube_points:
+        rotate_x = matrix_multiplication(rotationX_matrix, point)    #It doesn't matter in what order, but you've to multiply all
+        rotate_y = matrix_multiplication(rotationY_matrix, rotate_x)
+        rotate_z = matrix_multiplication(rotationZ_matrix, rotate_y)
+        
+        point2D = matrix_multiplication(projection_matrix, rotate_z)
+
+        x = (point2D[0][0] * scale) + WIDTH/2
+        y = (point2D[1][0] * scale) + HEIGHT/2
+
+        points[i] = (x,y)
+        i += 1
+
+        pygame.draw.circle(window, (50, 205, 50), (x, y), 5)
+
+
+    
+    connectPoints(0,1, points)
+    connectPoints(0,2, points)
+    connectPoints(4,5, points)
+    connectPoints(4,6, points)
+    
+    
+    connectPoints(1,3, points)
+    connectPoints(2,3, points)
+    connectPoints(5,7, points)
+    connectPoints(7,6, points)
+
+
+
+    connectPoints(0,4, points)
+    connectPoints(1,5, points)
+    connectPoints(2,6, points)
+    connectPoints(3,7, points)
+  
+
+
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    pygame.display.update()
